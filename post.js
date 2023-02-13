d508818e820cf015bcc0c43a38af0be06edba48a
ItVal <valentinhonvj1@gmail.com>
2023-02-13 15:36:56 +0100

    config & add post paranoid model

---
+++ post.js
@@ -0,0 +1,46 @@
+const Sequelize = require("sequelize");
+const { DataTypes, Op } = Sequelize;
+const dotenv = require("dotenv");
+dotenv.config();
+
+
+//connexion à la DB
+const sequelize = new Sequelize(
+  "tuto_sequelize",
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    dialect: "mysql",
+    host: "localhost",
+  }
+);
+
+//async await method
+const myFunction = async () => {
+  await sequelize.authenticate();
+  console.log("connexion à la database reussi");
+};
+myFunction();
+
+//modèle
+module.exports = (sequelize, DataTypes) => {
+    const Post = sequelize.define("Post", {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      title: {
+        type: DataTypes.STRING,
+      },
+      author: {
+        type: DataTypes.STRING,
+      },
+      content: {
+        type: DataTypes.STRING,
+      },
+    }, {
+      paranoid: true
+    });
+    return Post;
+  };
